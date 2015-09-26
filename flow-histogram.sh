b41b49f741c29b5488c2bdc829cfac4d3c05f75b
Alex Gartrell <alexgartrell@gmail.com>
2015-09-26 01:43:59 -0700

    Add flow-histogram.sh script to produce histograms from tcp flows
    Useful to gather a quick snapshot of where traffic is going.

---
+++ flow-histogram.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+function show_help {
+    (
+	echo "Usage: $1 [-c PACKET_COUNT] [-n] [-f FILTER]"
+	echo "Arguments:"
+	echo "    -c PACKET_COUNT    Number of packets to count"
+	echo "    -n                 Do not resolve addresses"
+	echo "    -r                 Always try to resolve addresses"
+	echo "    -f FILTER          tcpdump filter"
+	echo "    -N                 Suppress tcpdump stderr"
+	echo "    -Q                 Flow direction: in, out, inout (default)"
+	echo "    -h                 Show this message"
+    ) >/dev/$1
+    exit $2
+}
+
+packet_count=1000
+resolve_addrs_flag=
+filter='tcp or udp'
+err='/dev/stderr'
+dir='inout'
+
+while getopts c:nhrf:NQ: opt
+do
+    case $opt in
+	c)
+	    packet_count=$OPTARG
+	    ;;
+	n)
+	    resolve_addrs_flag=-n
+	    ;;
+	r)
+	    resolve_addrs_flag=-r
+	    ;;
+	f)
+	    filter=$OPTARG
+	    ;;
+	N)
+	    err='/dev/null'
+	    ;;
+	Q)
+	    dir=$OPTARG
+	    ;;
+	h)
+	    show_help stdout 0
+	    ;;
+	\?)
+	    echo "Invalid option: -$OPTARG" >&2
+	    show_help stderr 2
+	    ;;
+    esac
+done
+
+tcpdump -c $packet_count -q -t $resolve_addrs_flag -q -Q $dir "${filter}" \
+	2>$err \
+    | awk '{
+            sub(/,$/, "", $5);
+            sub(/:$/, "", $4);
+            print $1 " " $2 " " $4 " " tolower($5)
+        }' | column -t \
+    | sort | uniq -c | sort -n -r
